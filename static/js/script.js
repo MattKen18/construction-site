0ee01ac8d8e6256784c601438bc635dd268b8ccd
Matthew Carby <matthewcarby18@gmail.com>
2022-11-13 14:39:10 -0500

    initial commit

---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+    $("#house-img").animate({
+        left: "+=50px",
+        opacity: "1",
+    }, "slow")
+});
+
+// $(document).ready(() => {
+//     $("p").show("slow");
+// });
+
+console.log("working");
